4d3b843ec412947719329415a236dd0826751d8c
Michael Mathews <micmath@gmail.com>
2013-05-18 17:13:51 +0100

    Integrates our unit tests with travis and composer better.

--- micmath/Formica.php
+++ micmath/Formica.php
@@ -7,10 +7,17 @@
  * @license   http://www.opensource.org/licenses/mit-license.php
  */
 
+use Sunra\PhpSimple\HtmlDomParser;
+
 /**
  * The Formica class. 
  */
 class Formica
 {
 
+    public static $dom;
+    
+    function __construct($config=null) {
+        self::$dom = HtmlDomParser::str_get_html('<div>this is a test</div>');
+    }
 }
--- unit-test/bootstrap.php
+++ unit-test/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once 'micmath/Formica.php';
+require_once 'vendor/autoload.php';
