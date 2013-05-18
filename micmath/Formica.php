aaf28b300adf30741306e4b1ae0989d51ff5a953
Michael Mathews <micmath@gmail.com>
2013-05-18 15:36:04 +0100

    Adds first unit test.

---
+++ micmath/Formica.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Formica: prefill your forms.
+ *
+ * @copyright Copyright (c) 2013, Micahel Mathews <micmath@gmail.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * The Formica class. 
+ */
+class Formica
+{
+
+}
---
+++ unit-test/constructor.test.php
@@ -0,0 +1,15 @@
+<?php
+
+class ConstructorTest extends PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+    }
+
+    public function tearDown() {
+    }
+
+    public function testCanConstructFormica() {
+        $formica = new Formica();
+        $this->assertEquals(is_object($formica), true);
+    }
+}
